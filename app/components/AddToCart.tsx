a8637a74cba77ef30719920c3437699707911e64
Sandeep Poudel <poudel.sandeep@cloco.co.jp>
2024-03-18 10:20:27 +0900

    client and server components. client side rendering and server side rendering

---
+++ app/components/AddToCart.tsx
@@ -0,0 +1,12 @@
+'use client';
+import React from 'react';
+
+const AddToCart = () => {
+    return (
+        <div>
+            <button onClick={() => console.log("cliked")} className='btn btn-primary'>Add to Cart</button>
+        </div>
+    );
+};
+
+export default AddToCart;
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
+import ProductCard from "@/app/components/ProductCard";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <h1>Hello World</h1>
         <Link href='users'>Go to User page.</Link>
+        <ProductCard/>
     </main>
   );
 }
